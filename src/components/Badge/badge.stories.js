8cc5a925e47a07f683a31cd16341a10ec9b9becb
JokerWon <1595672103@qq.com>
2020-06-23 23:23:04 +0800

    新增徽标组件

---
+++ src/components/Badge/badge.stories.js
@@ -0,0 +1,10 @@
+
+import React from "react";
+import Badge from ".";
+
+export default {
+  title: "Badge",
+  component: Badge
+};
+
+export const Default = () => <Badge>默认</Badge>;
---
+++ src/components/Badge/style.js
@@ -0,0 +1,6 @@
+
+import styled from "styled-components";
+
+const StyledBadge = styled.div``;
+
+export default StyledBadge;
